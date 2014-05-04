f9dd599be6c2f9f44f25bb8243392900c6bd645e
Malte Rohde <malte.rohde@flavoursys.com>
2014-05-04 15:23:31 +0200

    Initial import.

---
+++ src/assets/js/resume.js
@@ -0,0 +1,6 @@
+//= require ../../vendor/js/cryptojs-aes.min
+//= require ../../vendor/js/cryptojs-md5.min
+//= require ../../vendor/js/zepto.min
+//= require ../../vendor/js/zepto-fx.min
+//= require ../../vendor/js/handlebars.runtime.min
+//= require_tree .
